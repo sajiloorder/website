b809e5cb9e95280ce28a42f6a4779333032acb44
Surya Panday <suryapanday1.sp@outlook.com>
2025-05-30 19:26:27 +1000

    few changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,6 @@ export default function LandingPage() {
   return (
     <>
       {/* hero section */}
-
       {/* new item section */}
       {/* drinks section */}
     </>
---
+++ src/app/profile/orders/page.tsx
@@ -0,0 +1,3 @@
+export default function OrdersPage({ children }: { children: React.ReactNode }) {
+  return <>{children}</>;
+}
