d64dd0c84d7f33b6db5eaf4d7b8729e3b009228e
Surya Panday <suryapanday1.sp@outlook.com>
2025-05-30 19:06:16 +1000

    change navbar look

--- src/components/nav/Nav.tsx
+++ src/components/nav/Nav.tsx
@@ -1,26 +1,31 @@
 import Link from "next/link";
 import Logo from "../ui/logo/Logo";
+import { AiOutlineMenu } from "react-icons/ai";
+import { AiOutlineShoppingCart } from "react-icons/ai";
+import { AiOutlineUser } from "react-icons/ai";
 
 export default function Nav() {
   return (
-    <nav className="flex  flex-row justify-between px-5 py-2 ">
+    <nav className=" flex flex-row justify-between items-center px-5 py-4  ">
       {/* menu-icon */}
-      <div className="flex flex-row gap-2">
-        <div>
-          <button className="">Menu</button>
-        </div>
+      <AiOutlineMenu className="cursor-pointer  hover:text-blue-500" />
+      <div className="flex flex-row gap-2 justify-between items-center  ">
         {/* logo */}
         <Logo />
       </div>
-      <div className="flex flex-row gap-2">
+
+      {/* only for desktop */}
+      {/* <div className="hidden sm:flex flex-row gap-2 ">
+        <Link href="">example</Link>
+    
+      </div> */}
+
+      <div className="flex flex-row gap-2 ">
         {/* cart-icon */}
-        <div>
-          <button>Cart</button>
-        </div>
+        <AiOutlineShoppingCart className="cursor-pointer  hover:text-blue-500" />
+
         {/* user-icon */}
-        <div>
-          <button>User</button>
-        </div>
+        <AiOutlineUser className="cursor-pointer hover:text-blue-500" />
       </div>
     </nav>
   );
--- src/components/ui/logo/Logo.tsx
+++ src/components/ui/logo/Logo.tsx
@@ -1,3 +1,3 @@
 export default function Logo() {
-  return <div className="flex flex-row font-semibold text-red-500 ">Sajilo Order</div>;
+  return <div className="flex flex-row font-semibold text-black   hover:text-blue-500">Sajilo Order</div>;
 }
